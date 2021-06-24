05ee62a3a3de4a42921b995e4c825ed0ab2cb5ec
林旻豫 <joe20414zx@gmail.com>
2021-06-24 19:43:46 +0800

    水果

---
+++ test/Controllers/ExamController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
